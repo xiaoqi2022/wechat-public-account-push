faea6ac9022bbe9a1651fa795666fed4b8759ad0
xiaoqi2022 <112109387+xiaoqi2022@users.noreply.github.com>
2022-09-17 13:40:49 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -58,7 +58,7 @@ export const config = {
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
   USERS: [
-    //{
+    {
       // 想要发送的人的名字
       name: "黄小萍",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
@@ -76,24 +76,24 @@ export const config = {
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn",
       //    专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      //festivals: [
+      festivals: [
         //    注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        //{"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
-        //{"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
+        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         //    注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        //{"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        //{"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
       ],
       //    专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         //    在一起的日子
-        //{"keyword": "love_day", date: "2022-09-08"},
+        {"keyword": "love_day", date: "2022-09-08"},
         //    结婚纪念日
-        //{"keyword": "marry_day", date: "2022-09-09"},
+        {"keyword": "marry_day", date: "2022-09-09"},
         //    退伍日
-        //{"keyword": "ex_day", date: "2022-09-10"},
+        {"keyword": "ex_day", date: "2022-09-10"},
       ]
-    },//
+    },
     {
       name: "黄小萍",
       id: "o2x_L6Wb6iC3RudSesIJeJfs74nk",
