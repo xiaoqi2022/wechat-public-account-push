fc4b44efabcd4e6809be072aac7c8ef689b93d01
xiaoqi2022 <112109387+xiaoqi2022@users.noreply.github.com>
2022-09-17 14:35:45 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wxd30b1f9d747209dc",
+  APP_ID: "",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "f25c1cb981ab51e643d02ae941ad9a88",
+  APP_SECRET: "",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
