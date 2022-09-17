3704be45439e326635f534dc695dc91f64543520
xiaoqi2022 <112109387+xiaoqi2022@users.noreply.github.com>
2022-09-17 14:36:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -116,11 +116,7 @@ export const config = {
       horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
       festivals: [{"type": "*生日", "name": "马小柒", "year": "2001", "date": "11-10", isShowAge: true}],
-      customizedDateList: [{"keyword": "love_day", date: "2021-01-13"}
-                           {"keyword": "lover_prattle", contents: ["因为太喜欢你，所以看谁都像是情敌。",
-                                                                   "申请成为你爱里的永久居民。",
-                                                                   "你很傻，你很笨，可我还是很羡慕你，因为你有我",
-                                                                   "遇见你，就好像捡到了100斤的运气",]}],
+      customizedDateList: [{"keyword": "love_day", date: "2021-01-13"}],
     {
       name: "老婆3",
       id: "",
